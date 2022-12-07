b7d3fdaf5a98346ab1d9bfaac070d31c733bcdbc
christopherruano <112957957+christopherruano@users.noreply.github.com>
2022-12-07 11:49:44 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Moodsic
-
 Moodsic is a web-application dedicated to evaluating a user’s listening habits and returning an analysis of the user’s mood swings over a long, medium, and short term time period. Each time period listed previously presents an aggregation of user listening habits over the course of a set time: short is roughly a month; medium is up to 6 months; long parses over the user’s complete listening history. Mood or happiness versus sadness is measured by the valence of each individual song played by the user’s spotify account, a metric created by spotify themselves that measures the perceived negative emotion of a song. Spotify themselves say valence "describes the musical positiveness conveyed by a track. Tracks with high valence sound more positive (happy, cheerful, euphoric), while tracks with low valence sound more negative (sad, depressed, angry)".
 
 Moodsic’s application extends beyond retail-usage. Recent studies suggest a strong correlation between mood and music listening habits, stressing how emotional context can be derived from these individual specific listening habits. Moreover, further studies have explored a compounding effect of sad music on existing depression. As mental health awareness becomes a growing concern for society, Moodsic attempts to democratize self-diagnosis in lieu of further studies that offer more comprehensive conclusions (see articles below).
@@ -8,9 +6,8 @@ Users are first introduced to Moodsic via a landing page which introduces users 
 
 Use of Moodsic is slightly more complex than simple git repository download, navigation, and run command prompts. Local running on the effort of third-party developers requires an account on Spotfiy’s developers platform - Spotipy. Successful login, authentication, and navigation allows for two crucial pieces of information for application configuration: API secret key and client key - a unique sequence of alphanumerics that initializes API requests, data queries, and subsequent analysis. Please visit https://developer.spotify.com to learn more about application functionality, API usage, and documentation.
 
+Music habits and correlation with mental health status: https://www.sciencedaily.com/releases/2015/10/151022094959.htm 
 
-Music habits and correlation with mental health status: https://www.sciencedaily.com/releases/2015/10/151022094959.htm
-The power of sad, angry, and generally negative music to worsen symptoms of mental health: https://journals.sagepub.com/doi/full/10.1177/20592043211057217
+The power of sad, angry, and generally negative music to worsen symptoms of mental health: https://journals.sagepub.com/doi/full/10.1177/20592043211057217 
+
 Info on valence: https://community.spotify.com/t5/Spotify-for-Developers/Valence-as-a-measure-of-happiness/td-p/4385221
-
-Youtube link:
--- app.py
+++ app.py
@@ -62,16 +62,19 @@ def getHistory():
     series_long = pd.Series([int(happiness_percentage_long), int(happiness_percentage_short)])
     pct_change_long = series_long.pct_change()
     pct_change_rounded = round(pct_change_long[1], 2)
+    # calculate top 5 happy songs all-time and current
+    top5_long = top_five_generator(long_term_tracks)
+    top5_short = top_five_generator(short_term_tracks)
+    # calculate top 5 sad songs all-time and current
+    top5_long_sad = top_five_generator_sad(long_term_tracks)
+    top5_short_sad = top_five_generator_sad(short_term_tracks)
 
-    
-
-    return render_template("results.html", long=happiness_percentage_long, medium=happiness_percentage_medium, short=happiness_percentage_short, pct_change_rounded=pct_change_rounded)
+    return render_template("results.html", long=happiness_percentage_long, medium=happiness_percentage_medium, short=happiness_percentage_short, pct_change_rounded=pct_change_rounded, happytop5a=top5_long, happytop5c=top5_short, sadtop5a=top5_long_sad, sadtop5c=top5_short_sad)
 
 
 @app.route("/logout")
 def logout():
-    for key in list(session.keys()):
-        session.pop(key)
+    session.clear()
     return redirect(url_for('index', _external=True))
 
 
@@ -119,11 +122,13 @@ def calculate_average_valence(term_valence):
 
 
 def top_five_generator(tracks):
+    # initialize variables and get access from API
     token = get_token()
     spotify = spotipy.Spotify(auth=token['access_token'])
     valence = 0
     rank1, rank2, rank3, rank4, rank5 = 0, 0, 0, 0, 0
     rank1_id, rank2_id, rank3_id, rank4_id, rank5_id = 0, 0, 0, 0, 0
+    # set and fill ranks by traversing API outputs
     for item in tracks:
         track = item['id']
         track_features = spotify.audio_features(tracks=track)
@@ -141,20 +146,92 @@ def top_five_generator(tracks):
             rank3 = valence
             rank3_id = track
             continue
-        elif (valence > rank4)
+        elif (valence > rank4):
             rank4 = valence
             rank4_id = track
             continue
-        elif (valence > rank5)
+        elif (valence > rank5):
             rank5 = valence
             rank5_id = track
             continue
-    number1 = artist_song_puller(rank1_id)
-    number2 = artist_song_puller(rank2_id)
-    number3 = 
-    
+    # get each rank's information
+    number1 = song_puller(rank1_id)
+    number2 = song_puller(rank2_id)
+    number3 = song_puller(rank3_id)
+    number4 = song_puller(rank4_id)
+    number5 = song_puller(rank5_id)
 
-def artist_song_puller(id):
+    number1a = artist_puller(rank1_id)
+    number2a = artist_puller(rank2_id)
+    number3a = artist_puller(rank3_id)
+    number4a = artist_puller(rank4_id)
+    number5a = artist_puller(rank5_id)
+    # return value in list
+    top5_list = [number1, number2, number3, number4, number5, number1a, number2a, number3a, number4a, number5a]
+    
+    return top5_list
+
+
+def top_five_generator_sad(tracks):
     token = get_token()
     spotify = spotipy.Spotify(auth=token['access_token'])
-        
+    valence = 0
+    rank1, rank2, rank3, rank4, rank5 = 1, 1, 1, 1, 1
+    rank1_id, rank2_id, rank3_id, rank4_id, rank5_id = 0, 0, 0, 0, 0
+    for item in tracks:
+        track = item['id']
+        track_features = spotify.audio_features(tracks=track)
+        intermediary = track_features[0]
+        valence = intermediary['valence']
+        if (valence < rank5):
+            rank5 = valence
+            rank5_id = track
+            continue
+        elif (valence < rank4):
+            rank4 = valence
+            rank4_id = track
+            continue
+        elif (valence < rank3):
+            rank3 = valence
+            rank3_id = track
+            continue
+        elif (valence < rank2):
+            rank2 = valence
+            rank2_id = track
+            continue
+        elif (valence < rank1):
+            rank1 = valence
+            rank1_id = track
+            continue
+    # get each rank's information
+    number1 = song_puller(rank1_id)
+    number2 = song_puller(rank2_id)
+    number3 = song_puller(rank3_id)
+    number4 = song_puller(rank4_id)
+    number5 = song_puller(rank5_id)
+
+    number1a = artist_puller(rank1_id)
+    number2a = artist_puller(rank2_id)
+    number3a = artist_puller(rank3_id)
+    number4a = artist_puller(rank4_id)
+    number5a = artist_puller(rank5_id)
+
+    top5_list = [number1, number2, number3, number4, number5, number1a, number2a, number3a, number4a, number5a]
+    
+    return top5_list
+
+
+def song_puller(id):
+    token = get_token()
+    spotify = spotipy.Spotify(auth=token['access_token'])
+    song_title = spotify.track(id)['name']
+    return song_title
+
+
+def artist_puller(id):
+    token = get_token()
+    spotify = spotipy.Spotify(auth=token['access_token'])
+    artist2 = spotify.track(id)['artists']
+    artist1 = artist2[0]
+    artist = artist1["name"]
+    return artist
